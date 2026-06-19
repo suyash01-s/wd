b71650069fb3b60f60cbed2d9753b7b30e5cc069
Suyash Supekar <suyashsupekar01@gmail.com>
2026-06-19 11:52:35 +0530

    .

---
+++ js/string.js
@@ -0,0 +1,66 @@
+// string methods '' "" '"'
+
+let a="fortune"
+console.log(a);
+console.log(typeof(a));
+
+let b=a.charAt(a)
+console.log(b);
+
+let c=a.charCodeAt(a)
+console.log(c);
+
+let d=a.endsWith('e')
+console.log(d);
+
+let e=a.startsWith('f')
+console.log(e);
+
+let f=a.includes('z')
+console.log(f);
+
+let g=a.toLocaleLowerCase()
+console.log(g);
+
+let h=a.toLocaleUpperCase()
+console.log(h);
+
+let i=a.concat("cloude")
+console.log(i);
+
+let j=i.replace("fortune","cravita")
+console.log(j);
+
+let k=a.repeat(5)
+console.log(k);
+
+// number method
+// 34 50.10
+
+let ab=14;
+console.log(a);
+console.log(typeof(ab));
+
+let xy=57.787756450
+console.log(xy);
+console.log(typeof(xy));
+
+let yz=xy.toFixed(2)
+console.log(yz);
+
+let uv=ab.toString()
+console.log(uv);
+console.log(typeof(uv));
+
+// Boolean
+
+let m=true
+console.log(typeof(m));
+
+// null
+
+let n=null
+console.log(typeof(n));
+
+let p;
+console.log(p);
