3a69d5289fc3042cdb823e6ce83789b1a29e8cef
Suyash Supekar <suyashsupekar01@gmail.com>
2026-06-19 11:08:14 +0530

    .

---
+++ js/one.js
@@ -0,0 +1,5 @@
+console.log("this is javascript");
+    alert("this is alert");
+    confirm("this is also alert like ");
+    document.writeln("this is js");
+    document.write("this old document ");
