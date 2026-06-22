f3f59199856fedce51e7b21e4b416aec0de65151
Suyash Supekar <suyashsupekar01@gmail.com>
2026-06-22 15:54:57 +0530

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,51 +1,271 @@
-// named function
-// function add(a,b){
-//     console.log(a+b)
+// // named function
+// // function add(a,b){
+// //     console.log(a+b)
 
 
+// // }
+// // add(5,5)
+
+// // ananomous function
+
+// // function(a,b){
+// //      console.log(a+b)
+
+
+// // }
+// // (5,5)
+
+// // IIF
+// // (function(a,b){
+// //      console.log(a+b)
+
+
+// //  })
+// //  (5,5)
+
+// // function with expression
+// // let mul=function(s,s){
+// //     console.log(s*s)
+
+// // }
+// // mul(5,5)
+
+// // arrow function ()=>
+// // let mul=((r,s)=>{
+// //     console.log(r*s)
+// // })
+// // mul(10*10)
+
+// // nested function
+// function a() {
+
+//     let name = "MAM"
+
+//     function b() {
+//         console.log("Hello " + name)
+//     }
+
+//         b()
 // }
-// add(5,5)
 
-// ananomous function
+// a()
 
-// function(a,b){
-//      console.log(a+b)
-
-
+// NAMED FUNCTION
+// Example 1
+// function hello(){
+//     console.log("Hello");
 // }
-// (5,5)
-
-// IIF
-// (function(a,b){
-//      console.log(a+b)
+// hello();
 
 
-//  })
-//  (5,5)
-
-// function with expression
-// let mul=function(s,s){
-//     console.log(s*s)
-
+// // Example 2
+// function add(){
+//     console.log(10+20);
 // }
-// mul(5,5)
+// add();
 
-// arrow function ()=>
-// let mul=((r,s)=>{
-//     console.log(r*s)
-// })
-// mul(10*10)
 
-// nested function
-function a() {
+// // Example 3
+// function name(){
+//     console.log("Suyash");
+// }
+// name();
 
-    let name = "MAM"
 
-    function b() {
-        console.log("Hello " + name)
-    }
+// // Example 4
+// function show(){
+//     console.log("JavaScript");
+// }
+// show();
 
-        b()
-}
 
-a()
+// // Example 5
+// function greet(){
+//     console.log("Good Morning");
+// }
+// greet();
+
+// // Anonymous Function
+// // Example 1
+// let a = function(){
+//     console.log("Hello");
+// };
+// a();
+
+
+// // Example 2
+// let b = function(){
+//     console.log("Welcome");
+// };
+// b();
+
+
+// // Example 3
+// let c = function(){
+//     console.log("JS");
+// };
+// c();
+
+
+// // Example 4
+// let d = function(){
+//     console.log("Learning");
+// };
+// d();
+
+
+// // Example 5
+// let e = function(){
+//     console.log("Coding");
+// };
+// e();
+
+// // IIF
+// // Example 1
+// (function(){
+//     console.log("Hello");
+// })();
+
+
+// // Example 2
+// (function(){
+//     console.log("Welcome");
+// })();
+
+
+// // Example 3
+// (function(){
+//     console.log("JavaScript");
+// })();
+
+
+// // Example 4
+// (function(){
+//     console.log("IIFE Example");
+// })();
+
+
+// // Example 5
+// (function(){
+//     console.log("Function Run");
+// })();
+
+// // Function Expression
+// // Example 1
+// let hello = function(){
+//     console.log("Hello");
+// };
+// hello();
+
+
+// // Example 2
+// let add = function(){
+//     console.log(5+5);
+// };
+// add();
+
+
+// // Example 3
+// let msg = function(){
+//     console.log("Message");
+// };
+// msg();
+
+
+// // Example 4
+// let show = function(){
+//     console.log("Show Data");
+// };
+// show();
+
+
+// // Example 5
+// let test = function(){
+//     console.log("Testing");
+// };
+// test();
+
+// // Arrow Function
+// // Example 1
+// let hello = () => {
+//     console.log("Hello");
+// };
+// hello();
+
+
+// // Example 2
+// let add = () => {
+//     console.log(10+20);
+// };
+// add();
+
+
+// // Example 3
+// let name = () => {
+//     console.log("Suyash");
+// };
+// name();
+
+
+// // Example 4
+// let show = () => {
+//     console.log("Arrow Function");
+// };
+// show();
+
+
+// // Example 5
+// let greet = () => {
+//     console.log("Good Morning");
+// };
+// greet();
+
+// // Nested Function
+// // Example 1
+// function outer(){
+//     function inner(){
+//         console.log("Inner Function");
+//     }
+//     inner();
+// }
+// outer();
+
+
+// // Example 2
+// function user(){
+//     function name(){
+//         console.log("Suyash");
+//     }
+//     name();
+// }
+// user();
+
+
+// // Example 3
+// function first(){
+//     function second(){
+//         console.log("Nested");
+//     }
+//     second();
+// }
+// first();
+
+
+// // Example 4
+// function main(){
+//     function sub(){
+//         console.log("Hello JS");
+//     }
+//     sub();
+// }
+// main();
+
+
+// // Example 5
+// function start(){
+//     function run(){
+//         console.log("Running");
+//     }
+//     run();
+// }
+// start();
