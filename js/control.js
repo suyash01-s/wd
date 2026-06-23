24978cb8b2de88706f6d613f16ffa30f3c9c93a3
Suyash Supekar <suyashsupekar01@gmail.com>
2026-06-23 11:53:31 +0530

    .

---
+++ js/control.js
@@ -0,0 +1,96 @@
+// conditional
+
+// if-else
+
+// let num=prompt("enter a number")
+// if(num%2==0){
+//     console.log("this number is even");
+    
+// }    
+// else{
+//     console.log("this num is odd");
+    
+// // }
+// let num=prompt("enter a number")
+// if(num>=0){
+//     console.log("this number is positive");
+    
+// }    
+// else{
+//     console.log("this num is nigative");
+    
+// }
+
+// else if
+// let age=prompt("enter your age")
+// if(age==15){
+//     console.log("you are eligibele for SSC");
+    
+// }
+// else if(age==18){
+//     console.log("you are eligibele for HSC");
+// }
+// else if(age==21){
+//     console.log("you are eligibele for Degree");
+// }
+// else if(age==24){
+//     console.log("you are eligibele for Masters");
+// }
+// else{
+//      console.log("you can join fc");
+     
+// }
+
+// switch case
+
+// let time = Number(prompt("Enter a time"));
+// switch(time){
+//     case 6:
+//         console.log("good morning");
+//     break;
+
+//     case 12:
+//         console.log("good afternoon");
+//     break;
+    
+//     case 4:
+//         console.log("good evening");
+//     break;
+    
+//     case 10:
+//         console.log("good night");
+//     break;
+    
+//     default:
+//         console.log("invalid input");
+
+        
+// }
+
+//looping statement
+
+// for loop
+// for(let i=1;i<=10;i++){
+//     console.log("happy bady\n");
+//      document.writeln("happy bady");
+// }
+// for(let i = 1; i <= 10; i++){
+//     document.write("happy bady <br>");
+// }
+
+// while loop
+
+// let a=1; //initialization
+// while(a<=10){ //condition
+//     console.log("hello"); 
+//     a++;
+// }
+
+// do-while loop
+let b=1;
+do{
+    console.log(b);
+    b++;
+    
+}
+while(b<=5)
