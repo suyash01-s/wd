2c8aaeb7ec0ff159ad6101799d86b8628aaa9b65
Suyash Supekar <suyashsupekar01@gmail.com>
2026-06-24 11:07:56 +0530

    .

---
+++ js/dom.js
@@ -0,0 +1,23 @@
+//DOM
+// document.getElementById()
+let c=document.getElementById("h11")
+c.style.backgroundColor="yellow"
+c.style.color="red"
+c.style.textAlign="center"
+
+// document.getElementsByClassName()
+let a=document.getElementsByClassName("c1")
+for(let i=0;i<a.length;i++){
+    a[0].style.color="black"
+    a[1].style.backgroundColor="red"
+    a[i].style.font="bold"
+}
+// document.getElementsByTagName()
+let b=document.getElementsByTagName("span")
+for(let j=0;j<a.length;j++){
+    b[j].style.backgroundColor="black"
+    b[j].style.color="red"
+    b[j].style.font="italic"
+}
+// document.querySelector()
+// document.querySelectorAll()
